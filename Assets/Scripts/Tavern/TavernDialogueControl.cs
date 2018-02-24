6fe2d90ad3812d477ea9833fb9f64d5d120faccc
NeguliSimanis <simanism@inbox.lv>
2018-02-24 19:59:59 +0200

    no important changes
    just saved my work before resting for the day

---
+++ Assets/Scripts/Tavern/TavernDialogueControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernDialogueControl : MonoBehaviour {
+
+    /*
+     * TO-DO:
+     * move all inkeeper and shady guy dialogue logic to this script
+     */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
