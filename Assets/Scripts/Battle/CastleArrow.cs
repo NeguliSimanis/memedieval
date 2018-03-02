2bfdd4a15020bd43e4705577c3d3d78aaef006c2
NeguliSimanis <simanism@inbox.lv>
2018-03-02 23:38:41 +0200

    improved arrow physics

--- Assets/Scripts/Battle/CastleArrow.cs
+++ Assets/Scripts/Battle/CastleArrow.cs
@@ -3,51 +3,138 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CastleArrow : MonoBehaviour {
+    #region targetting
+    [SerializeField]
+    private string arrowTargetLocationTag = "Castle arrow target";
     [SerializeField]
     private string playerUnitTag = "Player unit";
     [SerializeField]
-    private float Speed;
-    private float SelfDestruct;
+    Transform[] targetArray;
     public Transform Target;
+    private bool isActive = true;
+
+    #endregion
+
+    #region properties
+    [SerializeField]
+    private float Speed;
     [SerializeField]
     private int damage;
+    [SerializeField]
+    float shootingAngle = 20f;
+    Rigidbody2D rigidbody2d;
+    #endregion
 
+    #region selfdestruction
+    private float arrowBirthTime;
+    private float disablingCooldown = 0.1f;
+    private float destructionDelay = 3f;
+    #endregion
 
     void Start()
     {
-        SelfDestruct = 0;
-        //gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(-1f,1f);
+        arrowBirthTime = Time.time;
+        SelectTarget();
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
+        rigidbody2d.velocity = CalculateArrowVelocity(shootingAngle);
     }
 
+    void SelectTarget()
+    {
+        // fill target array if empty
+        if (targetArray[0] == null)
+        {
+            GameObject[] tempArray = GameObject.FindGameObjectsWithTag(arrowTargetLocationTag);
+            int counter = 0;
+            foreach (GameObject obj in tempArray)
+            {
+                targetArray[counter] = obj.GetComponent<Transform>();
+                counter++;
+            }
+        }
+
+        // select a random target
+        int targetID = Random.Range(0, targetArray.Length);
+        Target = targetArray[targetID];
+    }
+
+    Vector2 CalculateArrowVelocity(float angle)
+    {
+
+        Vector2 targetDirection = new Vector2(Target.position.x - transform.position.x, Target.position.y - transform.position.y);
+
+        float heightDifference = targetDirection.y;
+
+        // retain only the horizontal direction
+        targetDirection.y = 0;
+
+        // get horizontal distance
+        float horizontalDistance = targetDirection.magnitude;
+
+        // convert angle to radians
+        float angleInRadians = angle * Mathf.Deg2Rad;
+
+        // set targetdirection to the elevation angle
+        targetDirection.y = horizontalDistance * Mathf.Tan(angleInRadians);
+
+        // correct for small height differences
+        if (Mathf.Tan(angleInRadians) == 0) Debug.Log("ERROR");
+        horizontalDistance += heightDifference / Mathf.Tan(angleInRadians);
+
+        // calculate the velocity magnitude
+        if (horizontalDistance * Physics.gravity.magnitude == 0) Debug.Log("ERROR");
+        if (Mathf.Sin(2 * angleInRadians) == 0) Debug.Log("ERROR");
+        var vel = Mathf.Sqrt(horizontalDistance * Physics.gravity.magnitude / Mathf.Sin(2 * angleInRadians));
+
+        if (vel * targetDirection.normalized == null) Debug.Log("error");
+
+        Vector2 result = vel * targetDirection.normalized;
+
+        //check for errors in result
+        if (float.IsNaN(result.x) || float.IsNaN(result.y))
+        {
+            Debug.Log("physics error");
+            return new Vector2(-5.268912f, 6.279245f); // return default value
+        }
+        else
+        {
+            //Debug.Log(result.x);
+            //Debug.Log(result.y);
+            return result;
+        }
+    }
 
     void Update()
     {
-        /*Vector3 dir = gameObject.GetComponent<Rigidbody2D>().velocity;
-        float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
-
-        if (Target == null)
+        if (!isActive)
         {
-            SelfDestruct += Time.deltaTime;
-            if (SelfDestruct >= 0.3f) GameObject.Destroy(gameObject);
+            if (Time.time >= arrowBirthTime + destructionDelay)
+                Destroy(gameObject);
             return;
         }
-        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, Target.transform.position) <= float.Epsilon)
-        {
-            GameObject.Destroy(gameObject);
-        }
+        // rotate the arrow according to its current velocit
+        Vector3 dir = gameObject.GetComponent<Rigidbody2D>().velocity;
+        float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("collision0");
-        if (other.gameObject.tag == playerUnitTag)
+        
+        if (other.gameObject.tag == playerUnitTag && isActive)
         {
-            Debug.Log("collision1");
+           
             Destroy(other.gameObject);
             Target.GetComponent<Health>().Damage(Attack.Type.Archer, damage);
         }
     }   
+
+    void OnCollisionEnter2D()
+    {
+        if (Time.time >= arrowBirthTime + disablingCooldown)
+        {
+            Destroy(rigidbody2d);
+            isActive = false;
+        }
+    }
 }
