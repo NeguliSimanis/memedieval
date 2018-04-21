60b1092c922c4f8737b29f04086a9b99b0a492f9
NeguliSimanis <simanism@inbox.lv>
2018-04-21 13:50:26 +0300

    Cooldown bars update with time

--- Assets/Scripts/ChampionAbilities.cs
+++ Assets/Scripts/ChampionAbilities.cs
@@ -16,21 +16,25 @@ public class ChampionAbilities : MonoBehaviour {
     bool canChargeAbility = true;
     bool isChargingAbility = false;
 
-    private float chargingDuration = 3f;
+    private float chargingDuration = 3f; 
+
     private float abilityEffectDuration = 4f;
     private float abilityAvailableDuration = 2f;
     private float abilityCooldown = 5f;
 
+    private float chargingStartTime;
     private float chargingEndsTime;
 
     [SerializeField]
     GameObject chargingBar;
+    Image chargingBarImage;
     Transform canvasTransform;
 
     void Start()
     {
         SetDefaultValues();
-        canvasTransform = gameObject.transform.Find("Canvas").GetComponent<Canvas>().transform; 
+        canvasTransform = gameObject.transform.Find("Canvas").GetComponent<Canvas>().transform;
+        chargingBarImage = chargingBar.GetComponent<Image>();
     }
 
     void SetDefaultValues()
@@ -52,35 +56,41 @@ public class ChampionAbilities : MonoBehaviour {
 
     void ChargeAbility()
     {
-        
-
         chargingBar.SetActive(true);
         
         canChargeAbility = false;
         isChargingAbility = true;
 
-        chargingEndsTime = Time.time + chargingDuration;
+        chargingStartTime = Time.time;
+        chargingEndsTime = chargingStartTime + chargingDuration;
 
         waypointFollower.Speed = 0;
         animator.speed = 0;
+    }
+
+    void EndCharging()
+    {
+        canChargeAbility = true;
+        isChargingAbility = false;
+        waypointFollower.Speed = defaultMoveSpeed;
+        animator.speed = defaultAnimSpeed;
+        chargingBar.SetActive(false);
+    }
+
+    void UpdateChargingBarImage()
+    {
+        chargingBarImage.fillAmount = (Time.time - chargingStartTime)/chargingDuration;
     }
 
     void Update()
     {
         if (isChargingAbility)
         {
-            //chargingBar.FillAreaByWidth(10, 10, chargingColor,10);
-            //chargingBar.FloodFillArea(1, 1, chargingColor, chargingCounter);
-            //chargingBar.Apply();
-         
 
+            UpdateChargingBarImage();
             if (Time.time > chargingEndsTime)
             {
-                canChargeAbility = true;
-                isChargingAbility = false;
-                waypointFollower.Speed = defaultMoveSpeed;
-                animator.speed = defaultAnimSpeed;
-                chargingBar.SetActive(false);
+                EndCharging();
             }
         }
     }
