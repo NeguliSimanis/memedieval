c441828ec82b791660e3b8f5cd6342e2306c8db9
NeguliSimanis <simanism@inbox.lv>
2018-05-25 00:06:27 +0300

    Fixed bug where changing scenes triggered death sfx

---
+++ Assets/Scripts/CreatePlayerProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayerProfile : MonoBehaviour {
+
+    [SerializeField]
+    GameObject playerProfilePrefab;
+    [SerializeField]
+    string playerProfileTag = "Player profile";
+
+	void Awake()
+    {
+        if (GameObject.FindGameObjectWithTag(playerProfileTag) == null)
+            Instantiate(playerProfilePrefab);
+    }
+}
