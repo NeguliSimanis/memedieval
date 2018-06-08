1be754947b429481a7f3ac70767684e5ebbd4327
Stormskuggan <thunder.lord@hotmail.com>
2018-06-09 00:22:15 +0200

    Victorysound, taverndudes, broken arrow sound, Audio Rebalance

--- Assets/Scripts/MusicControl.cs
+++ Assets/Scripts/MusicControl.cs
@@ -11,7 +11,9 @@ public class MusicControl : MonoBehaviour {
 	private Health enemyCastleHealth;
     private Health playerCastleHealth;
     private FMODUnity.StudioEventEmitter music;
-    
+
+	[SerializeField] private FMODUnity.StudioEventEmitter victory;
+	[SerializeField] private FMODUnity.StudioEventEmitter lose;
 
     void Awake()
     {
@@ -25,13 +27,13 @@ public class MusicControl : MonoBehaviour {
 	{
 
 
-        DontDestroyOnLoad(this.gameObject);
+//        DontDestroyOnLoad(gameObject);
 
         music = GetComponent<FMODUnity.StudioEventEmitter> ();
 
 		music.SetParameter ("Transition", 0f);
 
-        UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        //UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
     }
 	
 	// Update is called once per frame
@@ -43,26 +45,26 @@ public class MusicControl : MonoBehaviour {
 
 		
     }
-    private void SceneManager_sceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
-    {
-        //throw new System.NotImplementedException();
-
-        if (scene.name == "Castle")
-        {
-            return;
-        }
-
-        else
-        {
-            Destroy(gameObject);
-            Debug.Log("Music Cleared");
-
-        }
-    }
-    private void OnDestroy()
-    {
-        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
-    }
+//    private void SceneManager_sceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+//    {
+//        //throw new System.NotImplementedException();
+//
+//        if (scene.name == "Castle")
+//        {
+//            return;
+//        }
+//
+//        else
+//        {
+//            Destroy(gameObject);
+//            Debug.Log("Music Cleared");
+//
+//        }
+//    }
+//    private void OnDestroy()
+//    {
+//        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+//    }
     private void BattleMonitorForChange()
     {
         try
@@ -71,17 +73,18 @@ public class MusicControl : MonoBehaviour {
             {
 
                 enemyCastleHealth = GameObject.FindGameObjectWithTag("EnemyCastle").GetComponent<Health>();
-                //Debug.Log (enemyCastleHealth.CurrentHealth);
+                Debug.Log (enemyCastleHealth.CurrentHealth);
 
 
-                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.90d && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth)
+                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.90f && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth)
                     music.SetParameter("Transition", 1f);
-                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.60d && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth * 0.89d)
+                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.60f && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth * 0.89f)
                     music.SetParameter("Transition", 2f);
-                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.10d && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth * 0.59d)
+                if (enemyCastleHealth.CurrentHealth > enemyCastleHealth.MaximumHealth * 0.10f && enemyCastleHealth.CurrentHealth < enemyCastleHealth.MaximumHealth * 0.59f)
                     music.SetParameter("Transition", 3f);
                 if (enemyCastleHealth.CurrentHealth <= 0)
-                    music.SetParameter("Transition", 4f);
+					Debug.Log("Koolt2");
+					victory.Play();
 
             }
             if (GameObject.FindGameObjectWithTag("Player castle").activeInHierarchy)
@@ -90,7 +93,8 @@ public class MusicControl : MonoBehaviour {
 
                 if (playerCastleHealth.CurrentHealth <= 0 && enemyCastleHealth.CurrentHealth > 0)
                 {
-                    music.SetParameter("Transition", 5f);
+					Debug.Log("Koolt");
+					//lose.Play();
                 }
             }
         }
