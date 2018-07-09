7aedbe55870fc7ef23709a0ff25a3f453e135bf2
NeguliSimanis <simanism@inbox.lv>
2018-07-09 15:50:58 +0300

    0.1.6 - exit confirmation window

---
+++ Assets/Scripts/Battle/TimeControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour {
+
+    /// <summary>
+    /// manages unit movement etc. when game is paused or the game speed is changed
+    /// </summary>
+
+    bool isGamePaused = false;
+
+    public void Pause()
+    {
+        if (isGamePaused)
+        {
+            return;
+        }
+        isGamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Unpause()
+    {
+        if (!isGamePaused)
+        {
+            return;
+        }
+        isGamePaused = false;
+        Time.timeScale = 1f;
+    }
+}
--- Assets/Scripts/QuitController.cs
+++ Assets/Scripts/QuitController.cs
@@ -2,19 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// opens confirmation window when ESCAPE pressed
+/// exits game when ESCAPE pressed twice or action is confirmed in the window
+/// </summary>
+
 public class QuitController : MonoBehaviour
 {
+    [SerializeField]
+    GameObject quitGamePopop;
 
-    /*
-    void OnApplicationQuit()
-    {
-        //Debug.Log("Application ending after " + Time.time + " seconds");
-    }*/
+    TimeControl timeController;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
-            Application.Quit();
+        {
+            if (quitGamePopop.activeInHierarchy == false)
+            {
+                PauseGame();
+                quitGamePopop.SetActive(true); 
+            }
+            else
+                Application.Quit();
+        }
+    }
+
+    void FindTimeControl()
+    {
+        GameObject player = PlayerProfile.Singleton.gameObject;
+        timeController = player.transform.Find("TimeControl").gameObject.GetComponent<TimeControl>();     
+    }
+
+    void PauseGame()
+    {
+        if (timeController == null)
+        {    
+            FindTimeControl();
+            if (timeController != null)
+            {
+                timeController.Pause();
+            }
+        }
+        else
+        {
+            timeController.Pause();
+        }
+    }
+
+    public void Unpause()
+    {
+        if (timeController == null)
+        {
+            FindTimeControl();
+            if (timeController != null)
+            {
+                timeController.Unpause();
+            }
+        }
+        else
+        {
+            timeController.Unpause();
+        }
 
     }
 }
